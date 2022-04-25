a1e3108abbbbc882b7b48a28a0dca3adcafb5c82
Shahosol <soltani.shaho@gmail.com>
2022-04-25 12:15:46 +0300

    assignment 3.4

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ app.get('/api/notes/:id', (request, response) => {
 });
 
 app.delete('/api/notes/:id', (request, response) => {
+  // assignment 3.4
   const id =  Number(request.params.id);
   if (id) {
     response.send("ok");
